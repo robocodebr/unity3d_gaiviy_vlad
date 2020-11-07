31637b86f73e4173abd3b6c02203653f162a8f31
robocodebr <robocodebr@gmail.com>
2020-11-07 16:56:24 +0300

    lesson10

---
+++ lesson10/Maze/Assets/Script/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    { 
+        if (Input.GetAxis("Horizontal")<0){
+            GetComponent<Rigidbody>().velocity += new Vector3(1, 0, 0);
+        
+    }else if (Input.GetAxis("Horizontal") > 0)
+        {
+            GetComponent<Rigidbody>().velocity -= new Vector3(1, 0, 0);
+        }
+
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            GetComponent<Rigidbody>().velocity += new Vector3(0, 0, 1);
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            GetComponent<Rigidbody>().velocity -= new Vector3(0, 0, 1);
+        }
+
+
+    }
+}
